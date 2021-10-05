fcc507195a8571056f93d6aa0eaaafba924be486
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-10-05 20:59:17 -0300

    Update App.js

--- segundo_hook/src/App.js
+++ segundo_hook/src/App.js
@@ -1,90 +1,110 @@
-import { useEffect, useState, useRef } from 'react';
-
-/* eslint-disable */
+import { useEffect, useRef, useState } from 'react';
 
 const isObjectEqual = (objA, objB) => {
-  return JSON.stringify(objA) === JSON.stringify(objB)
-}
+  return JSON.stringify(objA) === JSON.stringify(objB);
+};
 
 const useFetch = (url, options) => {
   const [result, setResult] = useState(null);
-  const [loading, setLoading] = useState(false)
-  const [shouldLoad, setShouldload] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [shouldLoad, setShouldLoad] = useState(false);
   const urlRef = useRef(url);
-  const optionsRef = useRef(options)
+  const optionsRef = useRef(options);
 
   useEffect(() => {
-    if (url !== urlRef.current){
-      urlRef.current = url
-      setShouldload((s) => !s)
-    }
-  },[url, options])
+    let changed = false;
 
-  useEffect(()=> {
-    console.log('EFFECT', new Date().toLocaleString())
-    console.log(optionsRef.current.headers)
+    if (!isObjectEqual(url, urlRef.current)) {
+      urlRef.current = url;
+      changed = true;
+    }
+
+    if (!isObjectEqual(options, optionsRef.current)) {
+      optionsRef.current = options;
+      changed = true;
+    }
+
+    if (changed) {
+      setShouldLoad((s) => !s);
+    }
+  }, [url, options]);
+
+  useEffect(() => {
+    let wait = false;
+    const controller = new AbortController();
+    const signal = controller.signal
+    
 
     setLoading(true);
 
     const fetchData = async () => {
-      await new Promise((r) => setTimeout(r, 3000))
+      await new Promise((r) => setTimeout(r, 1000));
 
       try {
-        const response = await fetch(urlRef.current, optionsRef.current)
-        const jsonResult = await response.json()
-        setResult(jsonResult)
-        setLoading(false)
+        const response = await fetch(urlRef.current, { signal, ...optionsRef.current });
+        const jsonResult = await response.json();
+
+        if (!wait) {
+          setResult(jsonResult);
+          setLoading(false);
+        }
       } catch (e) {
-        setLoading(false)
-        throw e
-      }      
-    }
+        if (!wait) {
+          setLoading(false);
+        }
+        console.log('My ERROR: ', e.message) ;
+      }
+    };
 
-      fetchData();
-   }, [shouldLoad])
+    fetchData();
 
-  return[result, loading]
-}
+    return () => {
+      wait = true;
+      controller.abort()
+    };
+  }, [shouldLoad]);
+
+  return [result, loading];
+};
 
 export const Home = () => {
-  const [postId, setPostId] = useState('')
-  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts' + postId, {
+  const [postId, setPostId] = useState('');
+  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts/' + postId, {
     headers: {
-      abc: '2000000',
+      abc: '1' + postId,
     },
-  })
+  });
 
   useEffect(() => {
-    console.log('ID do post', postId)
-  }, [postId])
+    
+  }, [postId]);
 
   if (loading) {
-    return <p>Loading...</p>
+    return <p>Loading...</p>;
   }
 
   const handleClick = (id) => {
     setPostId(id);
-  }
+  };
 
   if (!loading && result) {
+    // 1
     return (
       <div>
-        {result?.length > 0  ? (
-          result.map ((p) => ( 
-          <div key={`post-${p.id}`} onClick={() => handleClick(p.id)
-          }>
-            <p>{p.title}</p>
-          </div>      
-        )) 
+        {result?.length > 0 ? (
+          result.map((p) => (
+            <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
+              <p>{p.title}</p>
+            </div>
+          ))
         ) : (
-          <div  onClick={() => handleClick('')
-          }>
+          <div onClick={() => handleClick('')}>
             <p>{result.title}</p>
-          </div>      
-        )}        
+          </div>
+        )}
       </div>
-    )
+    );
   }
 
-  return <h1>Oi</h1>
-}
+  return <h1>Oi</h1>;
+};
